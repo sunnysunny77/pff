ce97ab9688c85c64991473927d17d79f453598df
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2021-08-12 23:09:18 +0800

    up

--- findability/blog/footer.php
+++ findability/blog/footer.php
@@ -2,10 +2,12 @@
 <footer class="footer text-center pb-5 pt-5" style="clear: both;">
 	<div class="container col-10 col-lg-6 text-right  mx-auto">
 		<div class="row">
-			<div class="col-5 col-lg-3 mr-3 p-0 ">
+			<div class="col-5 col-lg-4 mr-3 p-0 ">
 				<ul class="text-light p-0">
 					<li class="under text-left mb-2"><span class="dashicons dashicons-admin-links"></span>&nbsp;&nbsp;Site</li>
-					<li><a href="<?php echo wp_make_link_relative('https://localhost/blog'); ?>">BLOG</a></li>
+					<li>
+						<a href="<?php echo wp_make_link_relative('https://localhost/blog'); ?>">FINDABILITY BLOG</a>
+					</li>
 					<li>
 						<a href=<?php echo "https://" . $_SERVER['HTTP_HOST'] . "/index.html" ?>>HOME</a>
 					</li>
