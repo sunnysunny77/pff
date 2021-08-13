e55ce69218db20f18748ddf05416465fe0f475c5
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2021-08-13 18:12:07 +0800

    up

--- findability/blog/404.php
+++ findability/blog/404.php
@@ -2,8 +2,16 @@
 
 <div id="notfound" class="content-area col-sm-12 col-lg-8 mx-auto">
 
-	<h2>404</h2>
-	<p>Not Found :(</p>
+		<h2>404 not found</h2>
+		<h2>:(</h2>
+		<p> 
+		Regretfully this page could not be found.
+        <br>
+        Untill we find it, please try elsewhere.
+		</p>
+
+		<img width="100" height="100" src="https://perthfootballfixtures.online/findability/blog/wp-content/uploads/2021/08/404-error.png" atl="404 error icon"/>
+        
 
 </div>
 
