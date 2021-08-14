22e0f840d2b35fe1472b84f0f6f8dcc3ce711e82
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2021-08-15 00:51:44 +0800

    Update home.php

--- findability/blog/home.php
+++ findability/blog/home.php
@@ -6,7 +6,7 @@
 
     <?php while (have_posts()) : the_post(); ?>
 
-      <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
         <h1> <a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a> </h1>
 
@@ -28,7 +28,7 @@
 
         <?php edit_post_link(); ?>
 		           
-      </div> 
+  </article> 
 
     <?php endwhile; ?>
 
