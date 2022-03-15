623e0cffe99c717170a4ec8e995e34f7cdb0194a
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-03-15 21:14:15 +0800

    up

--- findability/blog/comments.php
+++ findability/blog/comments.php
@@ -16,17 +16,19 @@
 
   <?php comment_form(
 
-    $args = array(
-      'id_form'           => 'commentform',
-      'id_submit'         => 'commentsubmit',
-      'title_reply'       => 'Leave a Comment',
-      'title_reply_to'    => 'Leave a Comment to %s',
-      'cancel_reply_link' => 'Cancel Commnet',
-      'comment_field' =>  '<p><textarea placeholder="Start typing..." id="comment" class="form-control" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>',
-      'comment_notes_after' => '<p class="form-allowed-tags">' .
-        'You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes:' .
-        '</p><div class="alert alert-info">' . allowed_tags() . '</div>'
-    )
-  );
-  ?>
+$args = array(
+  'id_form'           => 'commentform',
+  'id_submit'         => 'commentsubmit',
+  'title_reply_before'   => '<h2 id="reply-title" class="comment-reply-title">',
+  'title_reply_after'    => '</h2>',
+  'title_reply'       => 'Leave a Comment',
+  'title_reply_to'    => 'Leave a Comment to %s',
+  'cancel_reply_link' => 'Cancel Commnet',
+  'comment_field' =>  '<p><textarea placeholder="Start typing..." id="comment" class="form-control" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>',
+  'comment_notes_after' => '<p class="form-allowed-tags">' .
+    'You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes:' .
+    '</p><p class="alert alert-info">' . allowed_tags() . '</p>'
+)
+);
+?>
 </div>
