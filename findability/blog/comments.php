42aae004f13f44b973027f87e265c14a1791f049
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-03-15 22:40:20 +0800

    up

--- findability/blog/comments.php
+++ findability/blog/comments.php
@@ -24,7 +24,7 @@ $args = array(
   'title_reply'       => 'Leave a Comment',
   'title_reply_to'    => 'Leave a Comment to %s',
   'cancel_reply_link' => 'Cancel Commnet',
-  'comment_field' =>  '<p><textarea placeholder="Start typing..." id="comment" class="form-control" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>',
+  'comment_field' =>  '<p><label for="comment" class="d-none">Comments</label><textarea placeholder="Start typing..." id="comment" class="form-control" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>',
   'comment_notes_after' => '<p class="form-allowed-tags">' .
     'You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes:' .
     '</p><p class="alert alert-info">' . allowed_tags() . '</p>'
